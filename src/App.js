7757b83135461a70b4188d28026c019e05096453
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-03-08 12:12:44 +0530

    Add LoginPage component and integrate it into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <LoginPage/>
     </div>
   );
 }
